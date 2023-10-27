eddb0beda96cd3fc11dd70cb291628a3e8bcc0be
EZ4Fanta <62417573+xfd997700@users.noreply.github.com>
2023-10-27 21:09:56 +0800

    root files

---
+++ README.md
@@ -0,0 +1,49 @@
+# MESPool: Molecular Edge Shrinkage Pooling for Hierarchical Molecular Representation Learning and Property Prediction
+
+Fanding Xu, Zhiwei Yang, Lizhuo Wang, Deyu Meng and Jiangang Long
+
+![figure1](.\main.bmp)
+
+## Requirements
+
+torch = 1.11.0
+torch_geometric = 2.0.4
+torch_scatter = 2.0.9
+torch_sparse = 0.6.13
+torchmetrics = 0.7.3
+
+## Usage
+
+There are three runnable Python scripts:
+
+
+| File   | Description     |
+| -------------- | ---- |
+| **run_egin.py** | Run the benchmark test with EGIN framework. |
+| **run_mes.py** | Run the benchmark test with MESPool framework. |
+| **visual_demo.py** | Show the pooling result of a given molecule on the trained model. |
+
+You can use -h or --help to list all the available parameters in the scripts and their explanations, e.g:
+
+```cmd
+python run_mes.py -h
+```
+
+All parameters in **run_egin.py** and **run_mes.py** have default settings, you can run them directly. The default benchmark dataset is BACE. You can change the task (dataset) by setting --dataset, e.g:
+
+```cmd
+python run_mes.py --dataset bbbp
+```
+
+The available dataset options include: ['bbbp', 'bace', 'clintox', 'hiv', 'tox21', 'esol', 'freesolv', 'lipo', 'muv', 'sider', 'covid', 'muta'].
+
+**Notice:** Before you start the benchmark test, please make sure the dataset has been put in ./datasets. You can download the benchmark we used in this work at https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/BBBP.csv (replace the BBBP in the hyperlink with another dataset name to download other datasets).
+
+**visual_demo.py** requires the SMILES and the name of molecule to be tested (the name is used for setting the file name of the pooling result image), e.g:
+
+```cmd
+python visual_demo.py --smiles N1(C)C2=C(N(C)C(=O)N(C)C2=O)N=C1 --name Caffeine
+```
+
+If you have any questions, you can discuss with us in **[Issues](https://github.com/xfd997700/MESPool/issues)**.
+
---
+++ visual_demo.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 17:18:14 2022
+
+@author: Fanding_Xu
+"""
+
+import argparse
+import torch
+from rdkit import RDLogger  
+from databuild import TaskConfig, set_seed
+
+from models.MUSE_model import MUSEPred
+import rdkit.Chem as Chem
+from rdkit.Chem.Draw import IPythonConsole, rdMolDraw2D
+from IPython.display import display, Image, SVG
+from databuild import GenerateData
+from databuild import comps_visualize
+
+if __name__ == "__main__":  
+    parser = argparse.ArgumentParser(description="Visual Demo", add_help=True)
+    
+    parser.add_argument('--smiles', type=str,
+                        help='smiles of the test molecule')
+    parser.add_argument('--name', type=str,
+                        help='name of the test molecule')
+    
+    parser.add_argument('--dataset', type=str, default='esol',
+                        help='which benchmark task to run (default: bace)')
+    parser.add_argument('--cuda_num', type=int, default=0,
+                        help='which gpu to use if any (-1 for cpu, default: 0)')
+    parser.add_argument('--seed', type=int, default=1234,
+                        help='random seed (default: 1234)')
+    parser.add_argument('--num_workers', type=int, default=0,
+                        help='Pytorch DataLoader num_workers (default: 0)')
+    
+    
+    parser.add_argument('--lin_before_conv', action="store_true",
+                        help='whether to set a linear layer before convolution layers(default: False)')
+    parser.add_argument('--dropout', type=float, default=0.2,
+                        help='dropout ratio (default: 0.2)')
+    parser.add_argument('--hidden_channels_x', type=int, default=128,
+                        help='model hidden channels for node embeddings (default: 128)')
+    parser.add_argument('--hidden_channels_e', type=int, default=128,
+                        help='model hidden channels for edge embeddings (default: 128)')
+    parser.add_argument('--threshold', type=int, default=0.5,
+                        help='threshold (default: 0)')
+    args = parser.parse_args()
+    
+    if args.dataset == 'esol':
+        args.threshold = 0.7
+    
+    
+    
+    is_cpu = True if args.cuda_num == -1 else False
+    set_seed(args.seed, is_cpu)
+    RDLogger.DisableLog('rdApp.*')
+    task = TaskConfig(args.dataset)
+    device = torch.device("cuda:" + str(args.cuda_num)) if not is_cpu and torch.cuda.is_available() else torch.device("cpu")
+    
+    model = MUSEPred(in_channels_x = 121,
+                     in_channels_e = 13,
+                     hidden_channels_x = args.hidden_channels_x,
+                     hidden_channels_e = args.hidden_channels_e,
+                     drop_out = args.dropout,
+                     num_classes = task.num_classes,
+                     lin_before_conv = args.lin_before_conv,
+                     threshold = args.threshold,).to(device)
+
+    model.load_state_dict(torch.load('buffer/' + args.dataset + '.pt'))
+
+    
+    mol = Chem.MolFromSmiles(args.smiles)
+    name = args.name
+
+    
+    data = GenerateData(mol, type_legnth=100).to(device)
+    with torch.no_grad():
+        model.eval()
+        pred = model(data)
+    
+    if args.dataset == 'esol':
+        print("Prediction: {:.4f}\n".format(pred.cpu().item()))
+    else:
+        print("Prediction: {:.4f}\n".format(pred.sigmoid().cpu().item()))
+        
+        
+    
+    multi_fig = True
+    imgform = 'svg'
+    imgs = comps_visualize(mol, model.comps,
+                           model.tars, data.edge_index,
+                           size=(800, 600), count_in_node_edges=True,
+                           multi_fig = multi_fig, form=imgform)
+    
+    if imgform == 'png':
+        if multi_fig:
+            for png in imgs:
+                display(Image(png))
+        else:
+            display(Image(imgs))
+    elif imgform == 'svg':
+        if multi_fig:
+            for idx, svg in enumerate(imgs):
+                display(SVG(svg))
+                file = 'results/svgs/' + args.dataset + '_' + name + '_' + str(idx) + '.svg'
+                with open(file, 'w') as f:
+                    f.write(svg)
+                    print("Image saved: " + file)
+        else:
+            display(SVG(imgs))
+            file = 'results/svgs/' + args.dataset + '_' + name + '_single.svg'
+            with open(file, 'w') as f:
+                f.write(svg)
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
